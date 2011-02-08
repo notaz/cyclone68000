58db92b945d17c6ebec0f4dc1c75fc4677318772
emudave <emudave@gmail.com>
2011-02-08 15:10:48 +0000

    license info for PicoDrive directory

--- PicoDrive/WinMain.cpp
+++ PicoDrive/WinMain.cpp
@@ -1,4 +1,11 @@
 
+// This file is part of the PicoDrive Megadrive Emulator
+
+// This code is licensed under the GNU General Public License version 2.0 and the MAME License.
+// You can choose the license that has the most advantages for you.
+
+// SVN repository can be found at http://code.google.com/p/cyclone68000/
+
 #include "stdafx.h"
 #include <stdarg.h>
 
